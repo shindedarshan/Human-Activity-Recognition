699bb63a5d7c6aa453e733a06034d31ef93e5aa2
bivasmaiti26 <bivasmaiti26@gmail.com>
2019-04-13 15:40:06 -0400

    Updated Parameters

--- Code/Model.py
+++ Code/Model.py
@@ -15,7 +15,7 @@ class Models:
     #Output: Saves Model File, prints accuracy
     @staticmethod
     def Run_Logistic_Regression_Model(X_train,X_test,y_train,y_test,filename,class_weights):
-        log_reg_model=LogisticRegression()
+        log_reg_model=LogisticRegression(max_iter=400,class_weight=class_weights)
         log_reg_model.fit(X_train,y_train)
         print("Train Accuracy",log_reg_model.score(X_train,y_train))
         print("Test Accuracy",log_reg_model.score(X_test,y_test))
@@ -36,7 +36,7 @@ class Models:
     #Output: Saves Model File, prints accuracy
     @staticmethod
     def Run_Decision_Tree(X_train,X_test,y_train,y_test,filename,class_weights):
-        clf = tree.DecisionTreeClassifier(class_weight=class_weights,max_features ='sqrt',min_impurity_decrease =1e-3)
+        clf = tree.DecisionTreeClassifier(class_weight=class_weights,max_features ='sqrt',min_impurity_decrease =1e-5,max_depth=300)
         clf.fit(X_train,y_train)
         print(clf.score(X_test,y_test))
         with open(filename,'wb+') as file:
@@ -70,19 +70,19 @@ class Models:
         parameters = {
             'application': 'multiclass',
             'metric': 'multi_logloss',
-            'num_class':n_classesknj,
-            'max_depth': 10,
+            'num_class':25,
+            'max_depth': 30,
             'is_unbalance': 'true',
             'boosting': 'gbdt',
             'num_leaves': 10,
             'feature_fraction': 0.7,
             'bagging_fraction': 0.7,
             'min_data_in_leaf': 1000,
-            'min_sum_hessian_in_leaf': 1e-3,
+            'min_sum_hessian_in_leaf': 1e-5,
             'bagging_freq': 20,
             'max_bin' : 256,
             'learning_rate': 0.05,
-            'verbose': 1
+            'verbose': 0
         }
 
         model = lgb.train(parameters,train_data,num_boost_round=200)
